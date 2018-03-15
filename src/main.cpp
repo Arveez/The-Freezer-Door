52bb24355efa65ad9fb0f09b43a1034072986312
Arveez <rv@arveez-news.com>
2018-03-15 09:50:56 +0100

    connects to wlan

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "connIds.c"
 
 void setup() {
+	Serial.begin(115200);
+	WiFi.begin(SSID, PSSWD);
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.print(".");
+		delay(500);
+	}
+	Serial.printf("connected to %s", SSID);
     // put your setup code here, to run once:
 }
 
